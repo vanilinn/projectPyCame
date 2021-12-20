074c00e76bbf68bb23fda19104d3c71214f5f53a
van <ivan.s.petukhov@yandex.ru>
2021-12-20 17:05:48 +0300

    Полностью переделал весь проект, тк раньше было более громоздко и ничего не работало. Пока не получается только сделать так, чтобы отрисовывалось каждый кадр, хоть и все правильно написано

--- main.py
+++ main.py
@@ -1,68 +1,153 @@
+import sys
+
 import pygame
+import os
+
+FPS = 50
+clock = pygame.time.Clock()
+WIDTH = 800
+HEIGHT = 600
+STEP = 10
+tile_width = tile_height = 50
+
+pygame.init()
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
 
 
-class Player:
-    def __init__(self, player_hp, player_damage, player_x_pos, player_y_pos):
-        self.player_hp = player_hp
-        self.player_damage = player_damage
-        self.player_x_pos = player_x_pos
-        self.player_y_pos = player_y_pos
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-class RangeMob:
-    def __init__(self, range_hp, range_damage, range_x_pos, range_y_pos):
-        self.range_hp = range_hp
-        self.range_damage = range_damage
-        self.range_x_pos = range_x_pos
-        self.range_y_pos = range_y_pos
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
-class MeleeMob:
-    def __init__(self, melee_hp, melee_damage, melee_x_pos, melee_y_pos):
-        self.range_hp = melee_hp
-        self.range_damage = melee_damage
-        self.range_x_pos = melee_x_pos
-        self.range_y_pos = melee_y_pos
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 50
+def start_screen():
+    intro_text = ["ЗАСТАВКА"]
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def draw(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                temp_color = (255, 255, 255)
-                pygame.draw.rect(screen, temp_color,
-                                 (self.left + x * self.cell_size, self.top + y * self.cell_size, self.cell_size,
-                                  self.cell_size))
-                pygame.draw.rect(screen, pygame.Color("white"),
-                                 (self.left + x * self.cell_size, self.top + y * self.cell_size, self.cell_size,
-                                  self.cell_size), 1)
-
-
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
-
-    board = Board(5, 7)
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        screen.fill((0, 0, 0))
-        board.draw(screen)
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
         pygame.display.flip()
-    pygame.quit()
+        clock.tick(FPS)
+
+
+player = None
+
+player_image = load_image("player.jpg")
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dx = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+start_screen()
+
+player, level_x, level_y = generate_level(load_level("level.txt"))
+camera = Camera()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.rect.x -= STEP
+            if event.key == pygame.K_RIGHT:
+                player.rect.x += STEP
+            if event.key == pygame.K_UP:
+                player.rect.y -= STEP
+            if event.key == pygame.K_DOWN:
+                player.rect.y += STEP
+
+    camera.update(player)
+
+    for sprite in all_sprites:
+        camera.apply(sprite)
+
+    screen.fill(pygame.Color(0, 0, 0))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+terminate()
