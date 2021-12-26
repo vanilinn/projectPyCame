dd6fcc5d937c4163617fcb2fe879f3b159d679a8
van <ivan.s.petukhov@yandex.ru>
2021-12-26 23:49:15 +0300

    Добавил начальную версию меню

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ FPS = 50
 clock = pygame.time.Clock()
 WIDTH = 800
 HEIGHT = 600
-STEP = 10
+STEP = 50
 tile_width = tile_height = 50
 
 pygame.init()
@@ -44,7 +44,7 @@ def start_screen():
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
+        string_rendered = font.render(line, True, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -63,18 +63,6 @@ def start_screen():
         clock.tick(FPS)
 
 
-player = None
-
-player_image = load_image("player.jpg")
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
@@ -86,21 +74,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
-class Camera:
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dx = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 def generate_level(level):
@@ -117,35 +91,118 @@ def generate_level(level):
     return new_player, x, y
 
 
-start_screen()
+def play():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    player.rect.x -= STEP
+                if event.key == pygame.K_d:
+                    player.rect.x += STEP
+                if event.key == pygame.K_w:
+                    player.rect.y -= STEP
+                if event.key == pygame.K_s:
+                    player.rect.y += STEP
+
+        screen.fill(pygame.Color(0, 0, 0))
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+
+        pygame.display.flip()
+
+
+def menu_window():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        menu.load_window()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+class Menu:
+    def __init__(self):
+        self.list_of_difficult = ['easy', 'medium', 'hard']
+        self.difficult = self.list_of_difficult[1]
+        self.level = 0
+
+        self.music = False
+        pygame.mixer.music.load("data/music.mp3")
+        pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(0.1)
+
+    def load_window(self):
+        fon = load_image('fon.jpg')
+        fon.get_rect()
+        screen.blit(fon, (2, 2))
+
+        image = load_image('frame_for_levels.png')
+        t_w = 7
+        t_h = 3
+        font = pygame.font.Font('data/font.ttf', 60)
+        num = 1
+        for i in range(t_h):
+            for j in range(t_w):
+                if not (j == 0 and i == 0):
+                    frame = image.get_rect()
+                    screen.blit(image, ((width - t_w * frame.width) // 2 + frame.width * j,
+                                        (height - t_h * frame.height) // 2 - 30 + frame.height * i))
+                    num_rendered = font.render(str(num), True, pygame.Color('#465945'))
+                    num_rect = num_rendered.get_rect()
+                    num_rect.y = (height - t_h * frame.height) // 2 - 21 + frame.height * i
+                    num_rect.x = (width - t_w * frame.width) // 2 + (12 if num >= 10 else 26) + frame.width * j
+                    screen.blit(num_rendered, num_rect)
+                    num += 1
+        image = load_image('frame_for_classic_level.png')
+        frame = image.get_rect()
+        screen.blit(image, ((width - t_w * frame.width) // 2,
+                            (height - t_h * frame.height) // 2 - 30))
+
+        # image = load_image('start_frame.png')
+        # screen.blit(image, (522, 444))
+        # font = pygame.font.Font('data/font.ttf', 55)
+        # start_rendered = font.render('Start', True, pygame.Color('#465945'))
+        # start_rect = start_rendered.get_rect()
+        # start_rect.y = 459
+        # start_rect.x = 538
+        # screen.blit(start_rendered, start_rect)
+
+        # screen.blit(load_image('music_on.png') if self.music else load_image('music_off.png'), (5, 2))
+        # screen.blit(load_image('title.png'), ((804 - 342) // 2, 10))
+
+    def sound(self):
+        self.music = not self.music
+        if self.music:
+            pygame.mixer.music.unpause()
+        else:
+            pygame.mixer.music.pause()
+
+
+player_image = load_image("player.jpg")
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+# start_screen()
 
 player, level_x, level_y = generate_level(load_level("level.txt"))
-camera = Camera()
+menu = Menu()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.rect.x -= STEP
-            if event.key == pygame.K_RIGHT:
-                player.rect.x += STEP
-            if event.key == pygame.K_UP:
-                player.rect.y -= STEP
-            if event.key == pygame.K_DOWN:
-                player.rect.y += STEP
+    menu_window()
 
-    camera.update(player)
-
-    for sprite in all_sprites:
-        camera.apply(sprite)
-
-    screen.fill(pygame.Color(0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-
+    play()
     pygame.display.flip()
 
     clock.tick(FPS)
